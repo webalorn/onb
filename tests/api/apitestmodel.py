759f2b2ebf92e62d8bd5c7bb173c1904c6394d0a
Theophane Vallaeys <webalorn@gmail.com>
2018-04-15 11:34:53 +0200

    Complete API user tests

--- tests/api/apitestmodel.py
+++ tests/api/apitestmodel.py
@@ -1,15 +1,25 @@
 from ..sqldb.sqldbmodel import SqldbTestModel
 import api.api, onb, json
+from engine.engine import Rand
+from werkzeug.wrappers import Headers
 
 class ApiTestModel(SqldbTestModel):
 	def setUp(self):
 		super().setUp()
 		self.app = onb.app.test_client()
 
+	### Restfull requests
+
 	def checkContentType(self, headers):
 		self.assertEqual(headers['Content-Type'], 'application/json')
 
 	def makeAppRequest(self, endpoint, method, checkCode, **kw):
+		headers = kw['headers'] if 'headers' in kw else Headers()
+		if 'token' in kw:
+			headers.add('Authorization', "Bearer " + kw['token'])
+			del kw['token']
+		kw['headers'] = headers
+
 		request = getattr(self.app, method)(endpoint, **kw)
 		self.checkContentType(request.headers)
 		self.assertEqual(request.status_code, checkCode)
@@ -18,7 +28,33 @@ class ApiTestModel(SqldbTestModel):
 	def app_get(self, endpoint, checkCode, **kw):
 		return self.makeAppRequest(endpoint, 'get', checkCode, **kw)
 
-	### Aseert functions
+	def app_post(self, endpoint, checkCode, **kw):
+		return self.makeAppRequest(endpoint, 'post', checkCode, **kw)
+
+	def app_put(self, endpoint, checkCode, **kw):
+		return self.makeAppRequest(endpoint, 'put', checkCode, **kw)
+
+	def app_delete(self, endpoint, checkCode, **kw):
+		return self.makeAppRequest(endpoint, 'delete', checkCode, **kw)
+
+	### User utils functions
+
+
+	def createUser(self, username, password=None):
+		if password:
+			password = User.hashPassword(password)
+		return User.create(username=username, password_hash=password).id
+
+	def createAnonymous(self):
+		return User.create().id
+
+	def newUsername(self):
+		return Rand.randomString()
+
+	def getUserToken(self, username, password):
+		return self.app_get('/user/auth', 200, data={'username': username, 'password': password})['auth_token']
+
+	### Assert functions
 
 	class AnyValue:
 		""" Used to validate any value in json schema """
--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -4,24 +4,28 @@ from api.common.errors import *
 import random
 
 class ApiUserTest(ApiTestModel):
-	def createUser(self, username, password=None):
-		if password:
-			password = User.hashPassword(password)
-		return User.create(username=username, password_hash=password).id
-
-	def authUser(self, username, password):
-		return self.app_get('/user/auth', 200, data={'username': username, 'password': password})['auth_token']
-
-	def test_getUserList(self):
+	def test_getUser(self):
 		data = self.app_get('/user/1', 200)
 		self.assertInJson({'id': 1, 'username': 'test_user', 'is_anonymous': False}, data)
 		self.assertNotInJson({'auth_token': self.AnyValue}, data)
 
+		self.app_get('/user', 401)
+
+		token = self.getUserToken('test_user', '1234')
+		profileData = self.app_get('/user', 200, token=token)
+		self.assertInJson({'id': 1, 'auth_token': self.AnyValue}, profileData)
+
+
+		self.assertEqual(self.app_get('/user/1', 200, token=token), data)
+
 	def test_auth(self):
+		# Test auth endpoint
 		self.assertInJson(
 			{"message": {'username': self.AnyValue}},
 			self.app_get('/user/auth', 400, data={})
 		)
+		with self.assertRaises(UserAuthError):
+			self.app_get('/user/auth', 400, data={'username': 'not_an_user', 'password': 1234})
 		with self.assertRaises(UserAuthError):
 			self.app_get('/user/auth', 400, data={'username': 'test_user', 'password': 123})
 
@@ -29,3 +33,47 @@ class ApiUserTest(ApiTestModel):
 			{"username": "test_user", "auth_token": self.AnyValue},
 			self.app_get('/user/auth', 200, data={'username': 'test_user', 'password': 1234})
 		)
+
+	def test_createUser(self):
+		self.assertInJson(
+			{"message": {'username': self.AnyValue}},
+			self.app_post('/user', 400, data={})
+		)
+		self.assertInJson(
+			{"message": {'password': self.AnyValue}},
+			self.app_post('/user', 400, data={'username': 'gobelin'})
+		)
+		with self.assertRaises(UserAlreadyExistsError):
+			self.app_post('/user', 400, data={'username': 'test_user', 'password': 1234})
+		
+		username = self.newUsername()
+		datas = self.app_post('/user', 200, data={'username': username, 'password': 1234})
+		self.assertInJson({'id':self.AnyValue, 'username': username, 'auth_token': self.AnyValue, 'is_anonymous': False}, datas)
+
+	def test_userAnonymous(self):
+		userDatas = self.app_post('/user/anonymous', 200, data={})
+		self.assertInJson({'id':self.AnyValue, 'username': None, 'auth_token': self.AnyValue, 'is_anonymous': True}, userDatas)
+		token = userDatas['auth_token']
+
+		# Claim account
+		self.assertInJson(
+			{"msg": "Missing Authorization Header"},
+			self.app_put('/user/anonymous', 401, data={})
+		)
+		self.app_put('/user/anonymous', 422, data={}, token="")
+		self.app_put('/user/anonymous', 422, data={}, token="1234")
+		
+		self.assertInJson(
+			{"message": {'username': self.AnyValue}},
+			self.app_put('/user/anonymous', 400, data={}, token=token)
+		)
+		with self.assertRaises(UserAlreadyExistsError):
+			claimDatas = self.app_put("/user/anonymous", 500, data={'username': 'test_user', 'password': 1234}, token=token)
+
+		username = self.newUsername()
+		self.assertInJson(
+			{"username": username, "auth_token": self.AnyValue, 'is_anonymous': False},
+			self.app_put('/user/anonymous', 200, data={'username': username, 'password': 1234}, token=token)
+		)
+		with self.assertRaises(NotFoundError):
+			claimDatas = self.app_put("/user/anonymous", 500, data={'username': username, 'password': 1234}, token=token)
